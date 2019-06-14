cb909007a30b0e1b3829f24059f6e1f35ba4871a
Chester Zara <chesterjzara@gmail.com>
2019-06-14 18:54:17 -0500

    experimenting with react router and gmaps apis

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+// React-Bootstrap Components
+import Navbar from 'react-bootstrap/Navbar'
+
+class Navigation extends Component {
+    render() {
+        return (
+            <Navbar bg="light" expand="md" >
+                <Navbar.Brand>Friendly Reviews</Navbar.Brand>
+            </Navbar>
+        )
+    }
+  }
+  export default About;
